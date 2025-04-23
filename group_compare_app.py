47766a892b9caa87d4e8808e306ef9866ce6b689
jhever-wed <john.hever@wedbush.com>
2025-04-23 13:34:58 -0500

    Add files via upload

--- group_compare_app.py
+++ group_compare_app.py
@@ -76,9 +76,9 @@ def compare_summaries(df1, df2):
                         mismatch = not np.isclose(val1, val2, equal_nan=True)
                     except:
                         mismatch = val1 != val2
-                if mismatch:
-                    logging.info(f"Mismatch in group={group}, field={col}, val1={val1}, val2={val2}")
-                    row_diff.append((col, val1, val2))
+        if mismatch:
+            logging.info(f"Mismatch in group={group}, field={col}, val1={val1}, val2={val2}")
+            row_diff.append((col, val1, val2))
             if row_diff:
                 diffs.append((group, row_diff))
     def generate_report(diffs):
