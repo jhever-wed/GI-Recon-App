7901114b6986143b0c953e99efecd92badea5b12
jhever-wed <john.hever@wedbush.com>
2025-05-05 10:35:03 -0500

    Add files via upload

--- cb_month_summary_app.py
+++ cb_month_summary_app.py
@@ -25,7 +25,7 @@ if atlantis_file and gmi_file:
 
     if df1 is not None and df2 is not None:
         df1 = df1[df1['RecordType'] == 'TP']
-        df1 = df1.rename(columns={'ExchangeEBCode': 'CB', 'TradeDate': 'Date', 'GiveUpAmt': 'Fee', 'ClearingAccount': 'Account'})
+        df1 = df1.rename(columns={'ExchangeEBCode': 'CB', 'TradeDate': 'Date', 'GiveUpAmt': 'Fee', 'ClearingAccount': 'Account', 'Quantity': 'Qty'})
         df2 = df2.rename(columns={'TGIVIF#': 'CB', 'TEDATE': 'Date', 'TFEE5': 'Fee', 'TQTY': 'Qty', 'Acct': 'Account'})
 
         df1['Date'] = pd.to_datetime(df1['Date'].astype(str), format='%Y%m%d', errors='coerce')
