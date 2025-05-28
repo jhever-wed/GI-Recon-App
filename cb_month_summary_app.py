97373e432be33cb8d6e1aca16b53f8122acc3051
jhever-wed <john.hever@wedbush.com>
2025-05-28 14:43:07 -0500

    Add files via upload

--- cb_month_summary_app.py
+++ cb_month_summary_app.py
@@ -9,8 +9,8 @@ uploaded_file1 = st.file_uploader("Upload Atlantis File")
 uploaded_file2 = st.file_uploader("Upload GMI File")
 
 if uploaded_file1 and uploaded_file2:
-    df1 = pd.read_csv(uploaded_file1)
-    df2 = pd.read_csv(uploaded_file2)
+    df1 = pd.read_csv(uploaded_file1, encoding='ISO-8859-1')
+    df2 = pd.read_csv(uploaded_file2, encoding='ISO-8859-1')
 
     # Simple summaries for testing
     df1['Qty'] = pd.to_numeric(df1['Qty'], errors='coerce')
